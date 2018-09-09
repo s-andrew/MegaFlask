e505a461baff2140cba297e05d49f6b4c6c7ad14
s-andrew <anso2393@yandex.ru>
2018-09-09 13:14:29 +0300

    Path 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 's-andrew'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
