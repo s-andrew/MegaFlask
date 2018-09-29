5f8bebf512f35863eef16afeaed750b2d39bc4ad
s-andrew <anso2393@yandex.ru>
2018-09-29 21:29:30 +0300

    Path 10: Email

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
+from time import time
 from hashlib import md5
 
+import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-from app import db, login
+from app import app, db, login
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -51,11 +53,23 @@ class User(db.Model, UserMixin):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        followed = Post.query\
-            .join(followers, (followers.c.followed_id == Post.user_id))\
+        followed = Post.query \
+            .join(followers, (followers.c.followed_id == Post.user_id)) \
             .filter(followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in},
+                          app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+        except Exception:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
