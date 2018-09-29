1c75e18b61439751eff0a84105181ab76dae3c75
s-andrew <anso2393@yandex.ru>
2018-09-29 23:47:50 +0300

    Path 11: Bootstrap restyle.

--- app/routes.py
+++ app/routes.py
@@ -89,7 +89,6 @@ def user(username):
     prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
-    # return render_template('user.html', user=user, posts=posts)
 
 
 @app.before_request
